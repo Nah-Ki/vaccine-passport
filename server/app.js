8be92bdc8362e588561f291a58d55fdc9a1bc612
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2024-09-30 18:00:00 +0530

    feat: docker compose build

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const appRoute = require('./src/routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: "*"}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
